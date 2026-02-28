dcdd5508468e1b5d246f20d9d091df8f2b886ad7
Manorath Bhatt <bhattmani10@gmail.com>
2026-02-28 23:52:16 +0545

    displayed task list on homepage

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Task
+
+def task_list(request):
+    tasks=Task.objects.all()  #fetches all from DB
+    return render(request, 'tasks/task_list.html',{'tasks':tasks})
+'''Task.objects.all(): This is Django's ORM (Object-Relational Mapper) talking to the database for you.
+
+render(): This function combines a template with a "context" dictionary (the {'tasks': tasks} part) to create the final HTML.'''
