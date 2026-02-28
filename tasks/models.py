d6025333702bff68aa07acf862c606f6f1a5c5c5
Manorath Bhatt <bhattmani10@gmail.com>
2026-02-28 23:36:08 +0545

    defined task model and registered in admin

---
+++ tasks/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):#models.Model: Tells Django this class is a database table.
+    title=models.CharField(max_length=200)
+    completed=models.BooleanField(default=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.title
+    #__str__: This is a "magic method" that ensures when we look at 
+    # a task in the admin panel, we see the title instead of something 
+    # generic like "Task object (1)".
